a7b1e02637d84e0362571bc103b3c5da41208baf
Daniel Zaleta <zaletadaniel@gmail.com>
2022-03-18 20:30:18 -0600

    CSS config

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -25,6 +25,14 @@ module.exports = {
                 use: [
                     { loader: 'html-loader'}
                 ]
+            },
+            {
+                test: /\.s[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
             }
         ]
     },
@@ -32,7 +40,10 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: './public/index.html',
             filename: './index.html',
-        })
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
     ],
     devServer: {
         static: path.join(__dirname, 'dist'),
